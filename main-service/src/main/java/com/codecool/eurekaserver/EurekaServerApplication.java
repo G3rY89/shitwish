faee645571abb9800b83ebd14cce90ccad05d5ff
Mate Toth-Csak <tothcsakmate@gmail.com>
2019-01-15 13:56:03 +0100

    Noticed Zuul and Eureka will be handled seperately, renamed files for this reason

--- main-service/src/main/java/com/codecool/eurekaserver/EurekaServerApplication.java
+++ main-service/src/main/java/com/codecool/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.codecool.mainservice;
+package com.codecool.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class MainServiceApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainServiceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
